6fcec8ee3f0c2b8109f4f2812587600f2e26018a
sean stephen brian <seanstephenbrian@gmail.com>
2022-12-21 15:56:14 -0500

    finish initial tests for players

--- src/js/player.test.js
+++ src/js/player.test.js
@@ -20,7 +20,27 @@ describe('successfully record player attacks', () => {
 
 describe('correctly recognize when all ships have sunk', () => {
 
-    test('player two ships are not all sunk', () => {
+    test('player two ships are NOT all sunk', () => {
+        expect(playerTwo.board.allSunk()).toBeFalsy();
+    });
 
+    test('player two ships ARE all sunk', () => {
+        playerOne.attack(playerTwo, [2, 3]);
+        playerOne.attack(playerTwo, [3, 3]);
+        playerOne.attack(playerTwo, [3, 7]);
+        playerOne.attack(playerTwo, [3, 8]);
+        playerOne.attack(playerTwo, [3, 9]);
+        playerOne.attack(playerTwo, [3, 10]);
+        playerOne.attack(playerTwo, [5, 5]);
+        playerOne.attack(playerTwo, [7, 5]);
+        playerOne.attack(playerTwo, [9, 3]);
+        playerOne.attack(playerTwo, [9, 4]);
+        playerOne.attack(playerTwo, [6, 9]);
+        playerOne.attack(playerTwo, [7, 9]);
+        playerOne.attack(playerTwo, [8, 9]);
+        playerOne.attack(playerTwo, [9, 9]);
+        playerOne.attack(playerTwo, [10, 9]);
+        
+        expect(playerTwo.board.allSunk()).toBeTruthy();
     });
 })
