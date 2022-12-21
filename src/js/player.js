b388e317b46401bf71839c3080225461d940f71c
sean stephen brian <seanstephenbrian@gmail.com>
2022-12-21 15:40:06 -0500

    begin writing player object

---
+++ src/js/player.js
@@ -0,0 +1,24 @@
+import Gameboard from "./gameboard";
+
+const Player = () => {
+
+    const board = Gameboard();
+
+        // 5 sample ships for testing purposes:
+        board.createShip([2, 3], [3, 3]);
+        board.createShip([3, 7], [3, 10]);
+        board.createShip([5, 5], [7, 5]);
+        board.createShip([6, 9], [10, 9]);
+        board.createShip([9, 2], [9, 4]);
+
+    function attack(otherPlayer, [x, y]) {
+        otherPlayer.Gameboard.receiveAttack(x, y);
+    }
+
+    return { 
+        board,
+        attack
+    }
+}
+
+export default Player;
