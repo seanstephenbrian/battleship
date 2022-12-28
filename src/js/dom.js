064c97b99e6b9f5e463afd24abc0a992310687de
sean stephen brian <seanstephenbrian@gmail.com>
2022-12-28 11:45:07 -0500

    work on styling grids, add grid titles

--- src/js/dom.js
+++ src/js/dom.js
@@ -26,18 +26,36 @@ import '../style.css';
         playerOneBoard.classList.add('player-one-board', 'gameboard');
         gameboards.appendChild(playerOneBoard);
 
+            const playerOneBoardTitle = document.createElement('div');
+            playerOneBoardTitle.classList.add('player-one-board-title', 'board-title');
+            playerOneBoardTitle.textContent = 'your grid:';
+            playerOneBoard.appendChild(playerOneBoardTitle);
+
+            const playerOneBoardSquares = document.createElement('div');
+            playerOneBoardSquares.classList.add('player-one-board-squares', 'board-squares');
+            playerOneBoard.appendChild(playerOneBoardSquares);
+
         const playerTwoBoard = document.createElement('div');
         playerTwoBoard.classList.add('player-two-board', 'gameboard');
         gameboards.appendChild(playerTwoBoard);
+
+            const playerTwoBoardTitle = document.createElement('div');
+            playerTwoBoardTitle.classList.add('player-two-board-title', 'board-title');
+            playerTwoBoardTitle.textContent = 'enemy grid:'
+            playerTwoBoard.appendChild(playerTwoBoardTitle);
+
+            const playerTwoBoardSquares = document.createElement('div');
+            playerTwoBoardSquares.classList.add('player-two-board-squares', 'board-squares');
+            playerTwoBoard.appendChild(playerTwoBoardSquares);
     })();
 
     (function createBoardGrids() {
-        const gameboards = document.querySelectorAll('.gameboard');
+        const gameboards = document.querySelectorAll('.board-squares');
         gameboards.forEach(board => {
             for (let y = 10; y >= 1; y--) {
                 for (let x = 1; x <= 10; x++) {
                     const gameSquare = document.createElement('div');
-                    gameSquare.classList.add(`game-square`, `${x}-${y}`);
+                    gameSquare.classList.add(`game-square`, `x-${x}`, `y-${y}`);
                     gameSquare.textContent = `${x}, ${y}`;
                     board.appendChild(gameSquare);
                 }
